d9d0c4e7db87f46fffe05c492f17ea0363be574f
adrianmarta <adymarta03@gmail.com>
2025-02-07 22:01:02 +0200

    details

--- humble-superhero-frontend/src/api.jsx
+++ humble-superhero-frontend/src/api.jsx
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/superheroes";
+const API_URL = "http://localhost:3000/superheroes";// the backend api url
 
-export const fetchSuperheroes = async () => {
+export const fetchSuperheroes = async () => {       // api call to the backend for listing superheroes
     const response = await axios.get(API_URL);
     return response.data;
 };
 
-export const addSuperhero = async (superhero) => {
+export const addSuperhero = async (superhero) => {      // api call for adding a superhero
     const response = await axios.post(API_URL, superhero);
     return response.data;
 };
